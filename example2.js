f95a5bec67c10765866273aa989ad2248aba5787
Digital Dimension <digitaldimension77@gmail.com>
2025-04-20 20:12:30 +0100

    Create example2.js

---
+++ example2.js
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>My First JS</title>
+  </head>
+  <body>
+    <h1>Hello from HTML!</h1>
+
+    <script>
+      document.body.innerHTML += "<p>Hello from JavaScript!</p>";
+    </script>
+  </body>
+</html>
